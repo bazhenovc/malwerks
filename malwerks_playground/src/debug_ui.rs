efab95b35e0238c32a382e91375692be6e2e816c
bazhenovc <bazhenovc@gmail.com>
2020-06-14 07:08:10 -0700

    Fixed build after imgui update
    `malwerks_playground`: Updated code to build with new imgui version

--- malwerks_playground/src/debug_ui.rs
+++ malwerks_playground/src/debug_ui.rs
@@ -20,37 +20,34 @@ pub fn show_debug_window<'a>(
         .always_auto_resize(true)
         .build(ui, || {
             // camera
-            if CollapsingHeader::new(ui, im_str!("Camera")).default_open(true).build() {
-                let camera = camera_state.get_camera_mut();
-                ui.text(ImString::from(format!("{:?}", camera.position)));
-                ui.text(ImString::from(format!("{:?}", camera.orientation)));
-                ui.text(ImString::from(format!("{:?}", camera.get_viewport())));
-                if ui.button(im_str!("Reset all"), [0.0, 0.0]) {
-                    camera.position = Default::default();
-                    camera.orientation = Default::default();
-                }
-                ui.same_line(0.0);
-                if ui.button(im_str!("Reset position"), [0.0, 0.0]) {
-                    camera.position = Default::default();
-                }
-                ui.same_line(0.0);
-                if ui.button(im_str!("Reset orientation"), [0.0, 0.0]) {
-                    camera.orientation = Default::default();
-                }
+            // if CollapsingHeader::new(ui, im_str!("Camera")).default_open(true).build() {
+            let camera = camera_state.get_camera_mut();
+            ui.text(ImString::from(format!("{:?}", camera.position)));
+            ui.text(ImString::from(format!("{:?}", camera.orientation)));
+            ui.text(ImString::from(format!("{:?}", camera.get_viewport())));
+            if ui.button(im_str!("Reset all"), [0.0, 0.0]) {
+                camera.position = Default::default();
+                camera.orientation = Default::default();
+            }
+            ui.same_line(0.0);
+            if ui.button(im_str!("Reset position"), [0.0, 0.0]) {
+                camera.position = Default::default();
+            }
+            ui.same_line(0.0);
+            if ui.button(im_str!("Reset orientation"), [0.0, 0.0]) {
+                camera.orientation = Default::default();
             }
             // input
-            if CollapsingHeader::new(ui, im_str!("Input")).default_open(true).build() {
+            ui.text_wrapped(im_str!(
+                "WASD for camera movement, right mouse click + drag to rotate, Space/LeftControl to move up/down"
+            ));
+            if gilrs.gamepads().count() > 0 {
                 ui.text_wrapped(im_str!(
-                    "WASD for camera movement, right mouse click + drag to rotate, Space/LeftControl to move up/down"
+                    "Right stick for camera movement, left stick to rotate, RB/LB to move up/down"
                 ));
-                if gilrs.gamepads().count() > 0 {
-                    ui.text_wrapped(im_str!(
-                        "Right stick for camera movement, left stick to rotate, RB/LB to move up/down"
-                    ));
 
-                    for (_id, gamepad) in gilrs.gamepads() {
-                        ui.text(ImString::from(format!("{} {:?}", gamepad.name(), gamepad.power_info())));
-                    }
+                for (_id, gamepad) in gilrs.gamepads() {
+                    ui.text(ImString::from(format!("{} {:?}", gamepad.name(), gamepad.power_info())));
                 }
             }
         });
