3b9fe94fa6f24e639efd66de4d49033c07958a86
bazhenovc <bazhenovc@gmail.com>
2020-06-14 18:18:41 -0700

    Ply optimizations
    `malwerks_ply`: Reduced memory overhead for structured elements, removed panics from the parsing code and return error instead, cleaned up the code.

---
+++ malwerks_ply/src/parse_ply.rs
@@ -0,0 +1,239 @@
+// Copyright (c) 2020 Kyrylo Bazhenov
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use crate::ply_structs::*;
+
+#[derive(Debug)]
+pub enum PlyError {
+    InvalidHeader,
+    InvalidData,
+}
+
+pub fn parse_ply<R>(input: &mut R) -> Result<Ply, PlyError>
+where
+    R: std::io::Read + std::io::Seek,
+{
+    // let mut buf_reader = std::io::BufReader::new(input);
+
+    // TODO: remove this once "bufreader_seek_relative" is available
+    let mut buf_reader = seek_bufread::BufReader::new(input);
+
+    let ply_header = match parse_header(&mut buf_reader) {
+        Some(header) => header,
+        None => return Err(PlyError::InvalidHeader),
+    };
+
+    let ply_data = match parse_data(&ply_header, &mut buf_reader) {
+        Ok(data) => data,
+        Err(_) => return Err(PlyError::InvalidData),
+    };
+
+    Ok(Ply { ply_header, ply_data })
+}
+
+fn parse_header<R>(buf_reader: &mut seek_bufread::BufReader<R>) -> Option<PlyHeader>
+where
+    R: std::io::Read + std::io::Seek,
+{
+    use std::io::BufRead;
+
+    let mut ply_format = PlyFormat::BinaryLittleEndian(1.0);
+    let mut ply_elements = Vec::new();
+
+    let mut temp_element_header: Option<PlyElementHeader> = None;
+    let mut element_index = 0;
+    let mut line = String::new();
+
+    loop {
+        match buf_reader.read_line(&mut line) {
+            Ok(_) => {
+                line.pop()?;
+                if line.starts_with("ply") || line.starts_with("comment") {
+                    // skip
+                } else if line.starts_with("format") {
+                    // format <name:str> <version:f32>
+                    let mut split = line.split_ascii_whitespace();
+
+                    let _format_tag = split.next()?;
+                    let format_name = split.next()?;
+                    let format_version = match split.next()?.parse() {
+                        Ok(v) => v,
+                        Err(_) => return None,
+                    };
+
+                    if format_name != "binary_little_endian" {
+                        return None;
+                    }
+
+                    ply_format = PlyFormat::BinaryLittleEndian(format_version);
+                } else if line.starts_with("element") {
+                    if let Some(mut element) = temp_element_header {
+                        element.properties.shrink_to_fit();
+                        ply_elements.push(element);
+                    }
+
+                    // element <name:str> <count:usize>
+                    let mut split = line.split_ascii_whitespace();
+
+                    let _element_tag = split.next()?;
+                    let element_name = split.next()?.to_owned();
+                    let element_count = match split.next()?.parse() {
+                        Ok(c) => c,
+                        Err(_) => return None,
+                    };
+
+                    temp_element_header = Some(PlyElementHeader {
+                        element_name,
+                        element_index,
+                        element_count,
+                        properties: Vec::new(),
+                    });
+                    element_index += 1;
+                } else if line.starts_with("property") {
+                    // property [<type> | "list" <type>] <type> <name:str>
+                    let mut split = line.split_ascii_whitespace();
+
+                    let _property_tag = split.next()?;
+                    let type_or_list = split.next()?;
+
+                    let (property_type, list_index_type) = if type_or_list == "list" {
+                        let list_index_type = PlyPropertyType::from_ply_str(split.next()?);
+                        let property_type = PlyPropertyType::from_ply_str(split.next()?);
+                        (property_type, Some(list_index_type))
+                    } else {
+                        let property_type = PlyPropertyType::from_ply_str(type_or_list);
+                        (property_type, None)
+                    };
+                    let property_name = split.next()?.to_owned();
+
+                    if let Some(element) = temp_element_header.as_mut() {
+                        element.properties.push(PlyPropertyHeader {
+                            property_name,
+                            property_type,
+                            list_index_type,
+                        });
+                    }
+                } else if line == "end_header" {
+                    if let Some(mut element) = temp_element_header {
+                        element.properties.shrink_to_fit();
+                        ply_elements.push(element);
+                    }
+
+                    break;
+                } else {
+                    log::error!("unknown ply tag: {:?}", line);
+                }
+                line.clear();
+            }
+
+            Err(_) => return None,
+        }
+    }
+
+    Some(PlyHeader {
+        ply_format,
+        ply_elements,
+    })
+}
+
+fn parse_data<R>(ply_header: &PlyHeader, buf_reader: &mut seek_bufread::BufReader<R>) -> Result<PlyData, std::io::Error>
+where
+    R: std::io::Read + std::io::Seek,
+{
+    use std::io::Read;
+    use std::io::Seek;
+
+    let mut ply_elements_data = Vec::with_capacity(ply_header.ply_elements.len());
+    for element in ply_header.ply_elements.iter() {
+        if element.contains_lists() {
+            // Thank you ply for an awesome design of list properties.
+            // Now I have to iterate over the entire data twice.
+            // This is at least 5 times slower compared to regular properties.
+
+            let mut per_element_offsets = Vec::new();
+            per_element_offsets.resize(element.element_count, 0);
+
+            let mut temporary_bytes = [0u8; 8];
+            let data_start = buf_reader.seek(std::io::SeekFrom::Current(0))?;
+
+            let mut element_buffer_size = 0;
+            for _element_id in 0..element.element_count {
+                for property in element.properties.iter() {
+                    if let Some(list_index_type) = property.list_index_type {
+                        let property_stride = property.property_type.stride();
+                        let list_index_stride = list_index_type.stride();
+
+                        buf_reader.read_exact(&mut temporary_bytes[0..list_index_stride])?;
+
+                        let element_stride = list_index_type.bytes_to_usize(&temporary_bytes) * property_stride;
+                        buf_reader.seek(std::io::SeekFrom::Current(element_stride as _))?;
+
+                        element_buffer_size += element_stride + list_index_stride;
+                    } else {
+                        let element_stride = property.property_type.stride();
+                        element_buffer_size += element_stride;
+                        buf_reader.seek(std::io::SeekFrom::Current(element_stride as _))?;
+                    }
+                }
+            }
+
+            let mut element_data = Vec::new();
+            element_data.resize(element_buffer_size, 0u8);
+
+            buf_reader.seek(std::io::SeekFrom::Start(data_start))?;
+
+            let mut element_data_start = 0;
+            for element_id in 0..element.element_count {
+                per_element_offsets[element_id] = element_data_start;
+                for property in element.properties.iter() {
+                    if let Some(list_index_type) = property.list_index_type {
+                        let property_stride = property.property_type.stride();
+                        let list_index_stride = list_index_type.stride();
+
+                        let list_index_slice =
+                            &mut element_data[element_data_start..element_data_start + list_index_stride];
+                        buf_reader.read_exact(list_index_slice)?;
+
+                        let element_stride = list_index_type.bytes_to_usize(&list_index_slice) * property_stride;
+                        element_data_start += list_index_stride;
+                        buf_reader
+                            .read_exact(&mut element_data[element_data_start..element_data_start + element_stride])?;
+
+                        element_data_start += element_stride;
+                    } else {
+                        let element_stride = property.property_type.stride();
+                        buf_reader
+                            .read_exact(&mut element_data[element_data_start..element_data_start + element_stride])?;
+
+                        element_data_start += element_stride;
+                    }
+                }
+            }
+
+            ply_elements_data.push(PlyElementData::Structured(PlyStructuredData {
+                per_element_offsets,
+                element_data,
+            }));
+        } else {
+            let element_stride = element.compute_non_list_stride();
+            let element_buffer_size = element.element_count * element_stride;
+
+            let mut element_data = Vec::new();
+            element_data.resize(element_buffer_size, 0u8);
+
+            buf_reader.read_exact(&mut element_data)?;
+            ply_elements_data.push(PlyElementData::Linear(PlyLinearData {
+                element_stride,
+                element_data,
+            }));
+        }
+    }
+
+    //match buf_reader
+    //    .read_exact(&mut [0u8; 1])
+    //    .expect_err("end of file test failed");
+
+    Ok(PlyData { ply_elements_data })
+}
---
+++ malwerks_ply/src/ply_structs.rs
@@ -0,0 +1,186 @@
+// Copyright (c) 2020 Kyrylo Bazhenov
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#[derive(Debug)]
+pub enum PlyFormat {
+    BinaryLittleEndian(f32),
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum PlyPropertyType {
+    Char,
+    UnsignedChar,
+    Short,
+    UnsignedShort,
+    Int,
+    UnsignedInt,
+    Float,
+    Double,
+}
+
+impl PlyPropertyType {
+    pub(crate) fn from_ply_str(s: &str) -> Self {
+        match s {
+            "char" => PlyPropertyType::Char,
+            "uchar" => PlyPropertyType::UnsignedChar,
+            "short" => PlyPropertyType::Short,
+            "ushort" => PlyPropertyType::UnsignedShort,
+            "int" => PlyPropertyType::Int,
+            "uint" => PlyPropertyType::UnsignedInt,
+            "float" => PlyPropertyType::Float,
+            "double" => PlyPropertyType::Double,
+            _ => panic!("unknown ply property type"),
+        }
+    }
+
+    pub(crate) fn bytes_to_usize(self, bytes: &[u8]) -> usize {
+        match self {
+            PlyPropertyType::Char => *bytemuck::from_bytes::<i8>(&bytes[0..1]) as _,
+            PlyPropertyType::UnsignedChar => *bytemuck::from_bytes::<u8>(&bytes[0..1]) as _,
+            PlyPropertyType::Short => *bytemuck::from_bytes::<i16>(&bytes[0..2]) as _,
+            PlyPropertyType::UnsignedShort => *bytemuck::from_bytes::<u16>(&bytes[0..2]) as _,
+            PlyPropertyType::Int => *bytemuck::from_bytes::<i32>(&bytes[0..4]) as _,
+            PlyPropertyType::UnsignedInt => *bytemuck::from_bytes::<u32>(&bytes[0..4]) as _,
+            PlyPropertyType::Float => *bytemuck::from_bytes::<f32>(&bytes[0..4]) as _,
+            PlyPropertyType::Double => *bytemuck::from_bytes::<f64>(&bytes[0..8]) as _,
+        }
+    }
+
+    pub(crate) fn stride(self) -> usize {
+        match self {
+            PlyPropertyType::Char => 1,
+            PlyPropertyType::UnsignedChar => 1,
+            PlyPropertyType::Short => 2,
+            PlyPropertyType::UnsignedShort => 2,
+            PlyPropertyType::Int => 4,
+            PlyPropertyType::UnsignedInt => 4,
+            PlyPropertyType::Float => 4,
+            PlyPropertyType::Double => 8,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PlyPropertyHeader {
+    pub property_name: String,
+    pub property_type: PlyPropertyType,
+    pub list_index_type: Option<PlyPropertyType>,
+}
+
+#[derive(Debug)]
+pub struct PlyElementHeader {
+    pub element_name: String,
+    pub element_index: usize,
+    pub element_count: usize,
+    pub properties: Vec<PlyPropertyHeader>,
+}
+
+impl PlyElementHeader {
+    pub(crate) fn compute_non_list_stride(&self) -> usize {
+        let mut stride = 0;
+        for property in self.properties.iter() {
+            stride += property.property_type.stride();
+        }
+        stride
+    }
+
+    pub(crate) fn contains_lists(&self) -> bool {
+        self.properties.iter().any(|e| e.list_index_type.is_some())
+    }
+}
+
+#[derive(Debug)]
+pub struct PlyHeader {
+    pub ply_format: PlyFormat,
+    pub ply_elements: Vec<PlyElementHeader>,
+}
+
+pub struct PlyLinearData {
+    pub(crate) element_stride: usize,
+    pub(crate) element_data: Vec<u8>,
+}
+
+pub struct PlyStructuredData {
+    pub(crate) per_element_offsets: Vec<usize>,
+    pub(crate) element_data: Vec<u8>,
+}
+
+pub enum PlyElementData {
+    Linear(PlyLinearData),
+    Structured(PlyStructuredData),
+}
+
+pub trait PlyElementAccess: bytemuck::Pod {
+    fn set_member_from_bytes(&mut self, member_index: usize, list_item_count: usize, slice: &[u8]);
+}
+
+pub struct PlyData {
+    pub(crate) ply_elements_data: Vec<PlyElementData>,
+}
+
+impl PlyData {
+    pub fn compute_used_memory(&self) -> usize {
+        let mut num_bytes = 0;
+        for element_data in self.ply_elements_data.iter() {
+            num_bytes += match element_data {
+                PlyElementData::Linear(linear_data) => {
+                    std::mem::size_of_val(&linear_data.element_stride) + linear_data.element_data.len()
+                }
+
+                PlyElementData::Structured(structured_data) => {
+                    std::mem::size_of_val(&structured_data.per_element_offsets[0])
+                        * structured_data.per_element_offsets.len()
+                        + structured_data.element_data.len()
+                }
+            };
+        }
+        num_bytes
+    }
+
+    pub fn fetch_element<T>(&self, element_header: &PlyElementHeader, element_index: usize, out_element: &mut T)
+    where
+        T: PlyElementAccess,
+    {
+        let element = &self.ply_elements_data[element_header.element_index];
+        match element {
+            PlyElementData::Linear(linear_data) => {
+                let data_start = element_index * linear_data.element_stride;
+                let data_end = data_start + linear_data.element_stride;
+                *out_element = *bytemuck::from_bytes(&linear_data.element_data[data_start..data_end]);
+            }
+
+            PlyElementData::Structured(structured_data) => {
+                let data_start = structured_data.per_element_offsets[element_index];
+                for (property_index, property) in element_header.properties.iter().enumerate() {
+                    if let Some(list_index_type) = property.list_index_type {
+                        let property_stride = property.property_type.stride();
+                        let list_index_stride = list_index_type.stride();
+
+                        let element_count = list_index_type
+                            .bytes_to_usize(&structured_data.element_data[data_start..data_start + list_index_stride]);
+
+                        out_element.set_member_from_bytes(
+                            property_index,
+                            element_count,
+                            &structured_data.element_data[data_start..data_start + element_count * property_stride],
+                        );
+                    } else {
+                        let element_stride = property.property_type.stride();
+                        out_element.set_member_from_bytes(
+                            property_index,
+                            0,
+                            &structured_data.element_data[data_start..data_start + element_stride],
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub struct Ply {
+    pub ply_header: PlyHeader,
+    pub ply_data: PlyData,
+}
