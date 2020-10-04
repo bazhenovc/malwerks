ae25f35ae25d755f93976d9e0746ab4ca9783007
bazhenovc <bazhenovc@gmail.com>
2020-10-04 14:43:38 -0700

    Tool dependencies cleanup
    `malwerks_gltf`: Moved `import_gltf` function to the crate
    `malwerks_tools`: Removed unused dependencies

--- malwerks_gltf/src/lib.rs
+++ malwerks_gltf/src/lib.rs
@@ -18,3 +18,39 @@ pub use gltf_meshes::*;
 pub use gltf_nodes::*;
 
 pub use global_resources::*;
+
+pub fn import_gltf(input_file: &std::path::Path) -> malwerks_resources::DiskStaticScenery {
+    let mut static_scenery = malwerks_resources::DiskStaticScenery {
+        images: Vec::new(),
+        buffers: Vec::new(),
+        meshes: Vec::new(),
+        samplers: Vec::new(),
+        material_layouts: Vec::new(),
+        material_instances: Vec::new(),
+        materials: Vec::new(),
+        buckets: Vec::new(),
+        environment_probes: Vec::new(),
+        global_resources: Default::default(),
+    };
+
+    let gltf = gltf::Gltf::open(&input_file).expect("failed to open gltf");
+    let base_path = std::path::Path::new(&input_file)
+        .parent()
+        .expect("failed to get file base path");
+
+    import_material_instances(&mut static_scenery, gltf.materials());
+    let primitive_remap_table = import_meshes(
+        &mut static_scenery,
+        &base_path,
+        gltf.buffers(),
+        gltf.views(),
+        gltf.meshes(),
+        gltf.materials(),
+    );
+    import_nodes(&mut static_scenery, primitive_remap_table, gltf.nodes());
+    import_images(&mut static_scenery, &base_path, gltf.materials(), gltf.images());
+    import_samplers(&mut static_scenery, gltf.samplers());
+    import_probes(&mut static_scenery, &base_path);
+    import_global_resources(&mut static_scenery, &base_path);
+    static_scenery
+}
--- malwerks_tools/src/import_gltf.rs
+++ malwerks_tools/src/import_gltf.rs
@@ -3,7 +3,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use malwerks_resources::*;
 use malwerks_gltf::*;
 
 #[derive(Debug, structopt::StructOpt)]
@@ -19,42 +18,6 @@ struct CommandLineOptions {
     compression_level: u32,
 }
 
-fn import_gltf(command_line: &CommandLineOptions) -> DiskStaticScenery {
-    let mut static_scenery = DiskStaticScenery {
-        images: Vec::new(),
-        buffers: Vec::new(),
-        meshes: Vec::new(),
-        samplers: Vec::new(),
-        material_layouts: Vec::new(),
-        material_instances: Vec::new(),
-        materials: Vec::new(),
-        buckets: Vec::new(),
-        environment_probes: Vec::new(),
-        global_resources: DiskGlobalResources::default(),
-    };
-
-    let gltf = gltf::Gltf::open(&command_line.input_file).expect("failed to open gltf");
-    let base_path = std::path::Path::new(&command_line.input_file)
-        .parent()
-        .expect("failed to get file base path");
-
-    import_material_instances(&mut static_scenery, gltf.materials());
-    let primitive_remap_table = import_meshes(
-        &mut static_scenery,
-        &base_path,
-        gltf.buffers(),
-        gltf.views(),
-        gltf.meshes(),
-        gltf.materials(),
-    );
-    import_nodes(&mut static_scenery, primitive_remap_table, gltf.nodes());
-    import_images(&mut static_scenery, &base_path, gltf.materials(), gltf.images());
-    import_samplers(&mut static_scenery, gltf.samplers());
-    import_probes(&mut static_scenery, &base_path);
-    import_global_resources(&mut static_scenery, &base_path);
-    static_scenery
-}
-
 fn main() {
     if std::env::var("CARGO_MANIFEST_DIR").is_ok() {
         std::env::set_var("RUST_LOG", "info");
@@ -67,7 +30,7 @@ fn main() {
         CommandLineOptions::from_args()
     };
 
-    let static_scenery = import_gltf(&command_line);
+    let static_scenery = import_gltf(&command_line.input_file);
 
     let output_file = if let Some(file) = command_line.output_file {
         file
